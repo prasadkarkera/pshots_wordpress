76bcfca53adbb227b5abed59a499816f9f0d6976
Prasad Karkera <prasad.karkera@gmail.com>
2018-04-30 06:16:01 +0000

    update theme file to add ld json

--- wp-content/themes/hitchcock/content.php
+++ wp-content/themes/hitchcock/content.php
@@ -23,3 +23,33 @@
 	</div>
 	
 </a><!-- .post -->
+<?php preg_match('/href="(.*)" rel/', get_the_category_list( ', ' ), $matches, PREG_OFFSET_CAPTURE); ?>
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BlogPosting",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "<?php echo $matches[1][0] ?>"
+  },
+  "headline": "<?php the_title(); ?>",
+  "image": [
+    "<?php echo $thumb['0']; ?>"
+   ],
+  "datePublished": "<?php the_time( get_option( 'date_format' ) ); ?>",
+  "dateModified": "<?php the_time( get_option( 'date_format' ) ); ?>",
+  "author": {
+    "@type": "Person",
+    "name": "Prasad Karkera"
+  },
+   "publisher": {
+    "@type": "Organization",
+    "name": "pshots",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://pshots.net/wp-content/uploads/2018/04/cropped-Signature_New_White_1.png"
+    }
+  },
+  "description": "<?php the_title(); ?>"
+}
+</script>
